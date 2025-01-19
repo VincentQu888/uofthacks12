e5c3c60d5163d4b428f1a698c450a8c9a73bcb98
vincentqu888 <vincentqu888@gmail.com>
2025-01-19 03:38:28 -0500

    initial commit

---
+++ llm.py
@@ -0,0 +1,38 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+def teacher_generator_llm(prompt):
+    chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile")
+    system = "Pretend you are a music theory teacher making music theory questions that MUST be about this information"
+    human = "{text}" 
+    prompt_template = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+    chain = prompt_template | chat
+    response = chain.invoke({"text": prompt}) 
+    return response.content
+
+
+def student_generator_llm(prompt):
+    chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile")
+    system = "Pretend you are a music theory student studying music theory questions to test your friends which MUST be about this information"
+    human = "{text}" 
+    prompt_template = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+    chain = prompt_template | chat
+    response = chain.invoke({"text": prompt}) 
+    return response.content
+
+
+def pro_generator_llm(prompt):
+    chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile")
+    system = "Pretend you are a music theory expert making music theory questions that MUST be about this information"
+    human = "{text}" 
+    prompt_template = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+    chain = prompt_template | chat
+    response = chain.invoke({"text": prompt}) 
+    return response.content
---
+++ routing.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, session
+from livereload import Server
+from llm import teacher_generator_llm, student_generator_llm, pro_generator_llm
+
+app = Flask(__name__)
+app.debug = True  
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/', methods=['POST'])
+def generate():
+    notes = request.form['notes-input']
+    accidentals = request.form['accidentals-input']
+    clef = request.form['clef-select']
+    time = request.form['time-input']
+    key = request.form['key-input']
+    toggle_accidentals = True if request.form.get('toggle-accidentals', 'off') == 'on' else False
+    toggle_perspective = True if request.form.get('toggle-perspective', 'off') == 'on' else False
+    
+    accidentalsPrompt = "random accidentals"
+    if not toggle_accidentals:
+        accidentalsPrompt = "no accidentals"
+    prompt = f"Use {clef} clef staff in {time} time signature with {accidentalsPrompt} to make a list of questions. No images."
+
+    return render_template(
+        'index.html',
+        notes = notes,
+        accidentals = accidentals,
+        clef = clef,
+        time = time,
+        key = key,
+        toggle_accidentals = toggle_accidentals,
+        toggle_perspective = toggle_perspective,
+        teacher_questions = teacher_generator_llm(prompt) if toggle_perspective else "",
+        student_questions = student_generator_llm(prompt) if toggle_perspective else "",
+        pro_questions = pro_generator_llm(prompt) if toggle_perspective else ""
+    )  
+
+
+server = Server(app.wsgi_app)
+server.watch("templates\index.html")
+server.serve()
