25c073a25dcfa7e6a84650aebf443fd95574ae1c
Vincent Qu <vincentqu888@gmail.com>
2025-01-19 03:56:23 -0500

    Update llm.py

--- llm.py
+++ llm.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 def teacher_generator_llm(prompt):
     chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile")
-    system = "Pretend you are a music theory teacher making music theory questions that MUST be about this information"
+    system = "Pretend you are a music theory teacher making music theory questions"
     human = "{text}" 
     prompt_template = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
@@ -18,7 +18,7 @@ def teacher_generator_llm(prompt):
 
 def student_generator_llm(prompt):
     chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile")
-    system = "Pretend you are a music theory student studying music theory questions to test your friends which MUST be about this information"
+    system = "Pretend you are a music theory student making music theory questions to test your friends"
     human = "{text}" 
     prompt_template = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
@@ -29,7 +29,7 @@ def student_generator_llm(prompt):
 
 def pro_generator_llm(prompt):
     chat = ChatGroq(temperature=0.5, model_name="llama-3.3-70b-versatile")
-    system = "Pretend you are a music theory expert making music theory questions that MUST be about this information"
+    system = "Pretend you are a music theory expert making music theory questions"
     human = "{text}" 
     prompt_template = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
 
